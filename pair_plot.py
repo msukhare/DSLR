8148c4463c60e1fa99967de514f95a08adc7d8cb
Mark SUKHAREV <msukhare@e3r6p8.42.fr>
2018-10-30 04:56:22 +0100

    pair_plot.py finit

--- pair_plot.py
+++ pair_plot.py
@@ -6,12 +6,13 @@
 #    By: msukhare <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/07/09 16:34:31 by msukhare          #+#    #+#              #
-#    Updated: 2018/07/10 09:46:58 by msukhare         ###   ########.fr        #
+#    Updated: 2018/10/30 04:55:34 by msukhare         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 import sys
 
 def check_col_str(data):
@@ -25,24 +26,25 @@ def read_file():
         data = pd.read_csv(sys.argv[1])
     except:
         sys.exit("File doesn't exist")
-    keys = []
+    data = data.drop(['First Name', 'Last Name', 'Birthday', 'Index'], axis=1)
+    data['Best Hand'] = data['Best Hand'].map({'Right': 0, 'Left': 1})
     for key in data:
-        if (check_col_str(data[key]) or key == "index"):
-            data.drop([key], axis = 1, inplace = True)
-        else:
-            keys.append(key)
-    return (data, keys)
+        if (key != "Hogwarts House"):
+            data.fillna(value={key: data[key].mean()}, inplace=True)
+    return (data)
 
 def main():
     if (len(sys.argv) <= 1):
         sys.exit("No name file")
     if (len(sys.argv) >= 3):
         sys.exit("too much file")
-    data, keys = read_file()
-    iris = sns.load_dataset("iris")
-    iris.head()
-    sns.pairplot(iris, hue='species', size=2.5)
-    sns.show()
+    data = read_file()
+    ##after see the pair plot, histogram, scatterplot i decied to move up best hand
+    ##, arytmencie, care of magic creature and astronomy
+    data = data.drop(['Best Hand', 'Astronomy', 'Arithmancy', 'Care of Magical Creatures'],\
+            axis=1)
+    sns.pairplot(data, hue="Hogwarts House")
+    plt.show()
 
 if __name__ == "__main__":
     main()
